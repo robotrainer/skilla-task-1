7a1db14448c3c8420bcd440edc1b754fd8a4ed53
robotrainer <asbu26@gmail.com>
2025-10-03 23:17:15 +0300

    подключена функция запроса получения списка звонков

--- src/components/pages/Calls/Calls.tsx
+++ src/components/pages/Calls/Calls.tsx
@@ -1,15 +1,30 @@
-// import { Icons, ArrowSVG } from "components/shared/icons";
+import { getList, type ICall, type IErrorResponse } from "api";
+import { useEffect, useState } from "react";
+
+// import { Icons, ArrowSVG } from "shared";
 
 import classes from "./Calls.module.scss";
 
 export const Calls = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [calls, setCalls] = useState<ICall[]>([]);
+
+  useEffect(() => {
+    getList({ date_start: "2025-09-01", date_end: "2025-10-03" })
+      .then((resp) => setCalls(resp.results))
+      .catch((error: IErrorResponse) => console.error(error.message, error.url))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  console.log("calls", calls);
+
   return (
     <div className={classes.calls}>
       <div className={classes.callsHeader}>{"Calls"}</div>
       {/* <Icons svg={<ArrowSVG color="var(--green)" rotate={180} />} /> */}
       <div className={classes.callsTableWrapper}>
         <div className={classes.callsTableInner}>
-          <div className={classes.callsTable}></div>
+          {isLoading ? <span>Loading...</span> : <div className={classes.callsTable} />}
         </div>
       </div>
     </div>
